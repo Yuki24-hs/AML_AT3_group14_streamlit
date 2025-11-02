1b2cc808171b7c6503f624a8e62dae65e1d0e745
yuki24-hs <yukthi.hosadurgashivalingegowda@student.uts.edu.au>
2025-11-02 18:33:17 +1100

    config change

--- students/25229384_Yukthi.py
+++ students/25229384_Yukthi.py
@@ -1,6 +1,29 @@
 import streamlit as st
+import pandas as pd
+import requests
+import altair as alt
+from datetime import datetime, timedelta
+
+API_URL = "https://at3-api-25229384-latest.onrender.com"
+API_URL_PREDICT = f"{API_URL}/predict/bitcoin"
+API_URL_7d = f"{API_URL}/predict/bitcoin/7d"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def run():
     st.title("AT3 Advanced Machine Learning Model 4")
+    
     
